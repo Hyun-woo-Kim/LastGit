f312756b89da4e24fe9b46d756a5839f2b8678df
KimJin <vksej1235@gmail.com>
2024-01-31 01:50:34 +0900

    24_01_31_0
    1. 피의 노동자 순찰 기능 구현
    - 일정 간격으로 순찰 및 일정 거리에 도달하면 멈췄다가 방향 바꿔서 다시 순찰

--- GrapperStart/Assets/Scripts/Enemy/BloodWorker/BloodWorkerAction.cs
+++ GrapperStart/Assets/Scripts/Enemy/BloodWorker/BloodWorkerAction.cs
@@ -6,11 +6,12 @@ public enum BloodState
 {
     STATE_PATROL,
     STATE_ATTACK,
+    STATE_STOP,
     
 }
 public class BloodWorkerAction : MonoBehaviour
 {
-    BloodState bloodState;
+    public BloodState bloodState;
     private BloodWorkerState myState;
 
     
@@ -20,15 +21,30 @@ public class BloodWorkerAction : MonoBehaviour
 
     void Start()
     {
-        this.bloodState = BloodState.STATE_PATROL; //기본적으로 순찰 상태로 변경.
+        this.bloodState = BloodState.STATE_PATROL; //기본적으로 시작하면 순찰 상태로 시작.
+
         setActionType(bloodState);
+
         myState = gameObject.AddComponent<BloodWorkerPatrol>();
-        myState.Initialize(bloodState); 
+        myState.Initialize(bloodState);
+
+        //순찰 기능 초기값 설정.
+        startPosition = transform.position;
+        patrolDirection = Vector2.right;
+
+      
+        
     }
 
+
+    public float patrolSpeed = 2.0f;
+    public float patrolDistance = 10.0f; // 순찰 거리
+
+    public Vector2 patrolDirection;
+    protected Vector2 startPosition;
     public void setActionType(BloodState _bloodState)
     {
-        
+       
 
         Component c = gameObject.GetComponent<BloodWorkerState>();
 
@@ -40,25 +56,81 @@ public class BloodWorkerAction : MonoBehaviour
         switch (_bloodState)
         {
             case BloodState.STATE_PATROL:
-                myState = gameObject.AddComponent<BloodWorkerPatrol>();
-                
-                myState.Patrol(_bloodState);
-              
+                myState = gameObject.AddComponent<BloodWorkerPatrol>();     
+                myState.Patrol(_bloodState,patrolSpeed,patrolDistance,patrolDirection,startPosition);
                 break;
-           
+            case BloodState.STATE_STOP:
+                myState = gameObject.AddComponent<BloodWorkerPatrol>();
+                myState.Stop(_bloodState);
+                StartCoroutine(PatTrolTurn()); 
+                break;
+
         }
     }
+    IEnumerator PatTrolTurn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        this.bloodState = BloodState.STATE_PATROL;
+       
 
+        patrolDirection *= -1;
+        startPosition.x = transform.position.x;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
 
- 
+        setActionType(BloodState.STATE_PATROL);
+    }
     void Update()
     {
         switch (bloodState)
         {
             case BloodState.STATE_PATROL:
-             
+                PatrolReaction(); //PATROL상태일 때는 반응 기능을 추가한다.
+                if (Mathf.Abs(transform.position.x - startPosition.x) >= patrolDistance)
+                {
+                    this.bloodState = BloodState.STATE_STOP;
+                    setActionType(BloodState.STATE_STOP);
+                }            
                 setActionType(BloodState.STATE_PATROL);
                 break;
+
         }
+    }
+
+    public GameObject ReactionPref;
+
+    public Transform patrolPos;
+    public Vector2 patrolBoxSize;
+
+    public bool isReact;
+    public Sprite reactSprite;
+    void PatrolReaction()
+    {
+        Transform thirdChild = transform.GetChild(3);
+        SpriteRenderer spriteRenderer = thirdChild.GetComponent<SpriteRenderer>();
+
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(patrolPos.position, patrolBoxSize, 0);
+
+        isReact = false;
+        spriteRenderer.sprite = null;
+
+
+        foreach (Collider2D collider in colliders)
+        {
+            if(collider.CompareTag("Player"))
+            {
+                isReact = true;
+                spriteRenderer.sprite = reactSprite;
+            }
+        }
+    }
+
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireCube(patrolPos.position, patrolBoxSize);//DrawWireCube(pos.position,boxsize)      
     }
 }
--- GrapperStart/Assets/Scripts/Enemy/BloodWorker/BloodWorkerPatrol.cs
+++ GrapperStart/Assets/Scripts/Enemy/BloodWorker/BloodWorkerPatrol.cs
@@ -4,41 +4,47 @@ using UnityEngine;
 
 public class BloodWorkerPatrol : BloodWorkerState
 {
+
+
+
+
+
   
-
-
-    
-  
-
 
     public override void Initialize(BloodState state)
     {
-        startPosition = transform.position;
-        Debug.Log("Init" + startPosition);
+
+       
 
     }
-    public float patrolSpeed = 2.0f;
-    public float patrolDistance = 10.0f; // 순찰 거리
 
-    private Vector2 startPosition;
-    private Vector2 patrolDirection = Vector2.right;
-    protected override void PatrolMovement(BloodState state)
+    protected  override void PatrolMovement(BloodState state, float patrolSpeed, float patrolDis, Vector2 patrolDir, Vector2 starPos)
     {
-        Debug.Log("PatrolMovement" + startPosition);
-        Debug.Log("PatrolMovement" + patrolDirection);
 
-        transform.Translate(patrolDirection * patrolSpeed * Time.deltaTime);
+        Debug.Log("Patrol startPosition " + starPos);
+        Debug.Log("Patrol patrolDirection " + patrolDir);
+        
+        //이동 애니메이션 추가. 
 
-        // 순찰 거리를 초과했을 경우 방향을 전환합니다.
-        if (Vector2.Distance(transform.position, startPosition) >= patrolDistance)
+        if (patrolDir == Vector2.right)
+        {       
+            transform.Translate(Vector2.right * patrolSpeed * Time.deltaTime);
+        }
+        else if (patrolDir == Vector2.left )
         {
-            // 방향을 반전시킵니다.
-            patrolDirection = -patrolDirection;
-            Debug.Log("Distance" + patrolDirection);
-            // 이동 거리 초기화
-            startPosition = transform.position;
+           
+            transform.Translate(Vector2.left * patrolSpeed * Time.deltaTime);
         }
 
+    
 
     }
+
+    protected override IEnumerator PatrolStop(BloodState state)
+    {
+        state = BloodState.STATE_STOP;
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log("정지 후 순찰");
+    }
+
 }
