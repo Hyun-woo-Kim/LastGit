723ca90bf932ef42e34758507743fa268d7dddc9
kjm0417 <154965306+kjm0417@users.noreply.github.com>
2024-02-16 23:33:52 +0900

    2024_02_16
    1. 퀘스트매니저 스크립트 생성 및 작성
    2. 퀘스트 스크립트 작성
    3. 퀘스트 상태 스크립트 작성

--- GrapperStart/Assets/1_Scripts/Quest/Quest.cs
+++ GrapperStart/Assets/1_Scripts/Quest/Quest.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quest : MonoBehaviour
+public class Quest 
 {
-    // Start is called before the first frame update
-    void Start()
+    public QuestInfo info; //QuestInfo스크립트에 대한 내용 가져오기
+
+    public QuestState state; //QuestState 가져옴
+
+    private int currentQuestStepIndex; //비공개 엔드 생성
+
+    public Quest(QuestInfo questInfo)
     {
-        
+        this.info = questInfo; //퀘스트 정보가져온것을 추가해주고
+        this.state = QuestState.Requirements_Not_Met; //퀘스트 상태를 처음에는 안받은 상태
+        this.currentQuestStepIndex = 0; // 0
     }
 
-    // Update is called once per frame
-    void Update()
+    //
+    public bool CurrentStepExists()
     {
-        
+        return (currentQuestStepIndex < info.questStepPrefabs.Length);
+    }
+
+    //
+    public void InstantiateCurrentQuestStep(Transform parentTransform)
+    {
+        GameObject questStepPrefab = GetCurrentQuestStepPrefab();
+        if(questStepPrefab != null)
+        {
+            Object.Instantiate<GameObject>(questStepPrefab, parentTransform);
+        }
+    }
+
+    //현재 단계의 퀘스트가 존재하지는 확인하고 아니면 다시 돌아가는 메서드
+    private GameObject GetCurrentQuestStepPrefab()
+    {
+        GameObject questStepPrefab = null; //퀘스트 프리팹을 null로
+
+        if(CurrentStepExists()) //CurrentStepExists메서드를 이용하여 현재 단계가 존재하는지 확인
+        {
+            questStepPrefab = info.questStepPrefabs[currentQuestStepIndex]; //ScriptableObject에서 조립식을 가져온다.
+
+        }
+        else
+        {
+            Debug.LogWarning("Quest 단계 프리팹을 가져오려고 시도했지만 stepIndex가 범위를 벗어났습니다. 이는" + "현재 단계가 없음을 나타냅니다." +
+                " QuestID = " + info.id + ", stepIndex= " + currentQuestStepIndex);
+        }
+        return questStepPrefab;
     }
 }
