9114ff544bdb8938b80563c2e7a1cd23eca02c09
kjm0417 <154965306+kjm0417@users.noreply.github.com>
2024-03-21 00:29:11 +0900

    2024_03_21
    대화 거의 구현되었는데 가져오는거에서 배열을 넘어섰다고 오류가 나긴하는데 작동에는 문제가 없다 하지만 왜 오류가 나는지 모르겠음 고쳐보겠음
    Interaction 상호작용부분 고치면 끝남

--- GrapperStart/Assets/1_Scripts/Manager/DatabaseManager.cs
+++ GrapperStart/Assets/1_Scripts/Manager/DatabaseManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class DatabaseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static DatabaseManager instance; //상속하여 쉽게 불러온다
+
+    [SerializeField]
+    string csv_FIleName;
+
+    //대화 내용 2,3,4줄을 꺼내와라 이런식
+    Dictionary<int, Dialogue> dialogueDIc = new Dictionary<int, Dialogue>(); //int값의 dialogue를 불러온다
+
+
+    public static bool isFinish= false; //저장이 되었냐 안됬냐
+
+    private void Awake()
     {
-        
+       if(instance == null)
+        {
+            instance =this; //DatabaseManager전체를 가져옴자신에
+            DialogueParser theParser = GetComponent<DialogueParser>();
+            Dialogue[] dialogues = theParser.Parse(csv_FIleName);
+            for(int i = 0; i<dialogues.Length; i++)
+            {
+                dialogueDIc.Add(i + 1, dialogues[i]);
+            }
+            isFinish = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //시작 끝
+    public Dialogue[] GetDialogue(int StartNum, int EndNum)
     {
-        
+        List<Dialogue> dialogueList = new List<Dialogue>();
+
+        for(int i = 0; i<=EndNum -StartNum; i++)
+        {
+            dialogueList.Add(dialogueDIc[StartNum + i]); //대화 줄 꺼내오기 처음부터 반복되는 수만큼
+
+        }
+
+        return dialogueList.ToArray();
     }
 }
