3f78172cb3325d4e3f711e68a442087d121de0ef
wls6189 <wls6189@naver.com>
2024-04-04 18:19:33 +0900

    24_4_4
    봄버맨

--- GrapperStart/Assets/1_Scripts/Object/BoomBerManHand.cs
+++ GrapperStart/Assets/1_Scripts/Object/BoomBerManHand.cs
@@ -1,53 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class BoomBerManHand : MonoBehaviour
 {
-    public ParticleSystem explosionParticlesPrefab; // 파티클 시스템을 할당할 변수
-    private ParticleSystem explosionParticles;
-    public float particleDuration = 1.0f;
-    private void OnEnable()
-    {
-        
-    }
+    public GameObject explosionParticlesPrefab; // 파티클 시스템을 할당할 변수
+
+    private GameObject explosion;
+    public float particleDel;
+
+    private bool isParticleSpawned = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isParticleSpawned)
         {
-            // 충돌한 위치를 파악
+            // 충돌한 위치를 파악하여 파티클 생성 및 삭제 코루틴 호출
             Vector3 collisionPosition = other.transform.position;
-
-            // 충돌 위치에 파티클 생성
-            if (explosionParticles == null)
-            {
-                // 파티클 시스템을 충돌 위치에 생성하고 재생
-                if (explosionParticlesPrefab != null)
-                {
-                    Debug.Log("이펙트");
-
-                    // 파티클 시스템 프리팹을 인스턴스화하여 충돌 위치에 생성
-                    explosionParticles = Instantiate(explosionParticlesPrefab, collisionPosition, Quaternion.identity);
-
-                    // 파티클 재생
-                    explosionParticles.Play();
-                }
-            }
+            StartCoroutine(InstantiateAndDestroyParticles(collisionPosition));
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    IEnumerator InstantiateAndDestroyParticles(Vector3 position)
     {
-        if (other.CompareTag("Player"))
-        {
-            // 충돌이 끝나면 파티클 제거
-            if (explosionParticles != null)
-            {
-                Destroy(explosionParticles.gameObject);
-                explosionParticles = null;
-            }
-        }
+        // 파티클 생성
+        explosion = Instantiate(explosionParticlesPrefab, position, Quaternion.identity);
+        isParticleSpawned = true;
+
+        // 대기 후 파티클 삭제
+        yield return new WaitForSeconds(particleDel);
+        DestroyExplosion();
     }
 
+    void DestroyExplosion()
+    {
+        // 파티클 삭제
+        Destroy(explosion);
+        isParticleSpawned = false;
+    }
 
 }
--- GrapperStart/Assets/1_Scripts/Player/ScriptableObject/PlayerData.cs
+++ GrapperStart/Assets/1_Scripts/Player/ScriptableObject/PlayerData.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Player Data")]
 public class PlayerData : ScriptableObject
 {
-    public int playerHp = 10;
+    public float playerHp = 10;
     public float curSpeed = 8;
 
-    public void DamagedHp(int amount)
+    public void DamagedHp(float amount)
     {
         playerHp -= amount;
     }
