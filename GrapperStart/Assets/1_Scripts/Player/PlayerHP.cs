e35727bbd72e33459236de707e7c32ddfb11b733
wls6189 <wls6189@naver.com>
2024-05-18 01:10:41 +0900

    24_5_18
    bm 연타공격시 점화 걸기. 테스트. 확인부탁

--- GrapperStart/Assets/1_Scripts/Player/PlayerHP.cs
+++ GrapperStart/Assets/1_Scripts/Player/PlayerHP.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -17,25 +18,59 @@ public class PlayerHP : MonoBehaviour
     public float maxMP;
 
     public bool isMPzero;
-     PlayerControllerRope player;
+    PlayerControllerRope player;
+    BManAction bm;
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerControllerRope>();
+        bm = FindAnyObjectByType<BManAction>();
         currentHP = maxHP;
         currentMP = maxMP;
 
         isMPzero = false;
     }
 
-    // Update is called once per frame
+    public float damageInterval = 1.0f;  // 피해를 줄 시간 간격 (초 단위)
+    public float totalDuration = 5.0f;   // 총 피해를 줄 지속 시간 (초 단위)
+    public float elapsedTime = 0.0f;
+    public float damageTimer = 0.0f;
+    public bool isDamaging = false;
     void Update()
     {
-        
-    }
+        UpdateHealthUI(); // UI 업데이트
 
+        if (bm.RepeatCount >= 1.0f)
+        {
+            // 점화 시작
+            isDamaging = true;
+           
+        }
+
+        if (isDamaging)
+        {
+            elapsedTime += Time.deltaTime;
+            damageTimer += Time.deltaTime;
+
+            if (damageTimer >= damageInterval)
+            {
+                StartCoroutine(Firedamage(0.1f));
+                damageTimer = 0.0f;
+            }
+
+            if (elapsedTime >= totalDuration)
+            {
+                isDamaging = false;
+            }
+        }
+    }
+    IEnumerator Firedamage(float damage)
+    {
+        currentHP -= damage;
+        yield return new WaitForSeconds(0.1f);
+    }
     public void TakeDamage(float damage)
     {
-
+        
         currentHP -= damage; // 데미지만큼 체력 감소
         currentHP = Mathf.Clamp(currentHP, 0f, maxHP); // 최소값은 0, 최대값은 최대 체력으로 제한
 
@@ -47,14 +82,14 @@ public class PlayerHP : MonoBehaviour
         else
         {
             player.animatorPlayer.SetTrigger("PlayerDeath");
-            Debug.Log("피 없음");
+            Debug.Log("피 없음"); 
         }
 
-        UpdateHealthUI(damage); // UI 업데이트
+       
 
     }
 
-    void UpdateHealthUI(float damage)
+    void UpdateHealthUI()
     {
         sliderHP.value = currentHP / maxHP;
     }
